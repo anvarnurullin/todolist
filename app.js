f97f5708eb922d570bbad7720f10930e60370bd0
anvarnurullin <87721486+anvarnurullin@users.noreply.github.com>
2021-12-30 20:22:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,171 @@
+(function() {
+    //Globals
+let todos = [];
+let users = [];
+let todoList = document.getElementById("todo-list");
+let userSelect = document.getElementById("user-todo");
+let form = document.querySelector("form");
+
+//Attach Events
+document.addEventListener("DOMContentLoaded", initApp);
+form.addEventListener("submit", handleSubmit);
+
+//Basic logic
+function getUserName(userId){
+    let user = users.find(u => u.id === userId);
+    return user.name;
+}
+
+function printTodo({id, userId, title, completed}){
+    let li = document.createElement("li");
+    li.className = "todo-item";
+    li.dataset.id = id;
+    li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(userId)}</b></span>`;
+
+    let status = document.createElement("input");
+    status.type = "checkbox";
+    status.checked = completed;
+    status.addEventListener("change", handleTodoChange);
+
+    let close = document.createElement("span");
+    close.innerHTML = "&times";
+    close.className = "close";
+    close.addEventListener("click", handleClose);
+
+    li.prepend(status);
+    li.append(close);
+
+    todoList.prepend(li);
+}
+
+function createUserOption(user){
+    let option = document.createElement("option");
+    option.value = user.id;
+    option.innerHTML = user.name;
+    userSelect.append(option);
+}
+
+function removeTodo(todoId){
+    todos = todos.filter(todo => todo.id !== todoId);
+
+    let todo = todoList.querySelector(`[data-id="${todoId}"]`);
+    todo.querySelector("input").removeEventListener("change", handleTodoChange);
+    todo.querySelector(".close").removeEventListener("click", handleClose);
+
+    todo.remove();
+}
+
+function alertError(error){
+    alert(error.message);
+}
+
+function initApp(){
+    Promise.all([getAllTodos(), getAllUsers()]).then(values => {
+        [todos, users] = values;
+
+        todos.forEach(todo => printTodo(todo));
+        users.forEach(user => createUserOption(user));
+    })
+};
+
+function handleSubmit(event){
+    event.preventDefault();
+
+    createTodo({
+        userId: Number(form.user.value),
+        title: form.todo.value,
+        completed: false,
+    })
+}
+
+function handleTodoChange(){
+    let todoId = this.parentElement.dataset.id;
+    let completed = this.checked;
+
+    toggleTodoComplete(todoId, completed);
+}
+
+function handleClose(){
+    let todoId = this.parentElement.dataset.id;
+    deleteTodo(todoId);
+}
+
+//Async
+async function getAllTodos(){
+    try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=15");
+        let data = await response.json();
+
+        return data;
+    } catch (error){
+        alertError(error);
+    }
+}
+
+async function getAllUsers(){
+    try{
+        let response = await fetch("https://jsonplaceholder.typicode.com/users?_limit=5");
+        let data = await response.json();
+
+        return data;
+    } catch (error) {
+        alertError(error);
+    }
+}
+
+async function createTodo(todo){
+    try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "POST",
+            body: JSON.stringify(todo),
+            headers: {
+                "Content-type": "application/json",
+            },
+        })
+    
+        let newTodo = await response.json();
+    
+        printTodo(newTodo); 
+    } catch (error){
+        alertError(error);
+    }
+}
+
+async function toggleTodoComplete(todoId, completed){
+    try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {
+            method: "PATCH",
+            body: JSON.stringify({completed}),
+            headers: {
+                "Content-type": "application/json",
+            },
+        }
+        );
+    
+        if (!response.ok){
+            throw new Error("Failed to connect with server! Please try again later.");
+        }
+    } catch (error) {
+        alertError(error);
+    }
+}
+
+async function deleteTodo(todoId){
+    try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`,
+        {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json",
+            },
+        });
+        if (response.ok){
+            removeTodo(todoId);
+        } else {
+            throw new Error("Failed to connect with server! Please try again later.");
+        }
+    } catch (error) {
+        alertError(error);
+    }
+}
+})()
